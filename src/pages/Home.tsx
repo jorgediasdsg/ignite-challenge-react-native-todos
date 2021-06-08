e1941dd84a1d047ff539cb1e7ea5b59ef48e31f4
Jorge Dias <jorge.dias@masonweb.com.br>
2021-06-08 06:15:49 -0300

    SOLVED: need open emulator

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,18 +11,39 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
-
+  const [tasks, setTasks] = useState<Task[]>([]);
+  
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+
+    if (!newTaskTitle) return;
+
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(oldState => [...oldState, data])
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    
+    const tasksList = tasks.map((current) => {
+      
+      if (current.id !== id) return current;
+
+      Object.assign(current, { done: !current.done });
+
+      return current;
+
+    });
+
+    setTasks(tasksList);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(oldState => oldState.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
